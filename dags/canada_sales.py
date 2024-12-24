7ba0c0d8ba8cf8430dac1c8d2b3d00ef531edeb7
cpelaezdc <163745780+cpelaezdc@users.noreply.github.com>
2024-12-23 21:35:57 -0500

    add changes in readme:

--- dags/canada_sales.py
+++ dags/canada_sales.py
@@ -155,28 +155,36 @@ def extract_sales(columns_to_load,**kwargs):
     print(f"Path files: {path_files}")
 
     # Get the oldest file in the folder bassed in the file name YYY-MM
-    file_to_process = find_oldest_file(path_files)
-    print(f"File to process: {file_to_process}")
+    try:
+        file_to_process = find_oldest_file(path_files)
+        print(f"File to process: {file_to_process}")
+            # load the file into a dataframe
+        df = pd.read_csv(file_to_process,usecols=columns_to_load)
+        # Get the number of rows
+        print(f'number of items before transform {str(df.shape[0])}')
+        print(f'columns: {list(df.columns)}')
+        print(f'the 5 first rows are: {df.head(5)}')
 
-    # load the file into a dataframe
-    df = pd.read_csv(file_to_process,usecols=columns_to_load)
-    # Get the number of rows
-    print(f'number of items before transform {str(df.shape[0])}')
-    print(f'columns: {list(df.columns)}')
-    print(f'the 5 first rows are: {df.head(5)}')
+        # Drop rows with null values
+        df = df.dropna()
+        # Drop duplicates
+        df = df.drop_duplicates()
+        # Get the number of rows
+        print(f'number of items after transform {str(df.shape[0])}')
+        print(f'the 5 first rows are: {df.head(5)}')
 
-    # Drop rows with null values
-    df = df.dropna()
-    # Drop duplicates
-    df = df.drop_duplicates()
-    # Get the number of rows
-    print(f'number of items after transform {str(df.shape[0])}')
-    print(f'the 5 first rows are: {df.head(5)}')
+        # Send the dataframe to XCom
+        kwargs['ti'].xcom_push(key='df_extract_sales', value=df.to_dict())
+        # send the file name to XCom
+        kwargs['ti'].xcom_push(key='file_name', value=file_to_process)  
 
-    # Send the dataframe to XCom
-    kwargs['ti'].xcom_push(key='df_extract_sales', value=df.to_dict())
-    # send the file name to XCom
-    kwargs['ti'].xcom_push(key='file_name', value=file_to_process)  
+        return True
+
+    except Exception as e:
+        print(f"There are not files")
+        print(f"An error occurred: {e}")
+        return False
+
 
 
 def transform_sales(column_mapping,prior_task,ti):
